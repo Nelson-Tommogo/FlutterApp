1d4436dcb1e26fb9f322125b4f2196ba02f0cc7e
Tommogo-coder <nelsontommogo9@gmail.com>
2023-07-15 09:58:57 +0300

    Dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:simpleapp/Color/colors.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('SmartMavuno App'),
-        centerTitle: true,
-        backgroundColor: ColorList.grey,
-      ),
-      body: Center(
-        child: Text(
-          "SmartMavuno",
-          style: TextStyle(fontSize: 40,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 12,
-          color: ColorList.red),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          // Add your onPressed code here!
-        },
-        child: Icon(Icons.navigation),
-        backgroundColor: ColorList.grey,
-      ),
-    ),
-  ));
-  class test extends StatelessWidget{
+void main() => runApp(MaterialApp(
+    home:Home()
+));
+
+  class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-  return container();
+  return Scaffold(
+    appBar: AppBar(
+      title: Text('SmartMavuno App'),
+      centerTitle: true,
+      backgroundColor: ColorList.grey,
+    ),
+    body: Center(
+      child: Text(
+        "SmartMavuno",
+        style: TextStyle(fontSize: 40,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 12,
+            color: ColorList.red),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {
+        // Add your onPressed code here!
+      },
+      child: Text("Click"),
+      backgroundColor: ColorList.grey,
+    ),
+  );
   }
+
   }
-}
+
